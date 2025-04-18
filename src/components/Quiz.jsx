086e4e3519246abfdd55e55339154c5faf013a7f
AqibNiazi <aqibjaved5201@gmail.com>
2025-04-18 18:40:48 +0500

    Questions and answered showed on the screen

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import Questions from "../questions";
 
 const Quiz = () => {
-  return <div>Quiz</div>;
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  const activeQuestionIndex = userAnswers.length;
+
+  const handleSelectedAnswer = (selectedAnswer) => {
+    setUserAnswers((prevAnswers) => [...prevAnswers, selectedAnswer]);
+  };
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{Questions[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {Questions[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectedAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Quiz;
